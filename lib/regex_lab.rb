7946e4e7caf60da06d1a52696bc8a7c2091bcb5c
Peter Stone <peter.stone.online@gmail.com>
2018-09-28 20:17:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,11 +1,11 @@
 def starts_with_a_vowel?(word)
   
   word.match(/^[aeiou]/i) ? true : false 
-  
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\Aun/)
+  
 end
 
 def words_five_letters_long(text)
