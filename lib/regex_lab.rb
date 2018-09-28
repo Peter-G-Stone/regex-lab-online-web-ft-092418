2b7d20a30931d21de8dc15dbc36ce7757cea5e8d
Peter Stone <peter.stone.online@gmail.com>
2018-09-28 20:12:52 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,12 @@
 def starts_with_a_vowel?(word)
-
+  if word.scan(/\A[aeiou]/i).empty?
+    false 
+  else true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\Aun/)
 end
 
 def words_five_letters_long(text)
